fa2209c31bc773a192c14af2ae43b01f19868d26
Daniel Morgan <me@danielmorgan.co.uk>
2017-07-31 14:16:43 +0100

    Quicksort

---
+++ quick_sort.py
@@ -0,0 +1,10 @@
+def quick_sort(list):
+    if (len(list) < 2):
+        return list
+    else:
+        pivot = list[0]
+        less = [i for i in list[1:] if i <= pivot]
+        greater = [i for i in list[1:] if i > pivot]
+        return quick_sort(less) + [pivot] + quick_sort(greater);
+
+print(quick_sort([5, 4, 3, 2, 1]))
